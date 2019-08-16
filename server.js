8d523eab7465f42cc09efb97b612095b3ed5c2a6
shoon0224 <dltkdgns204@naver.com>
2019-08-17 01:59:17 +0900

    서버 재구축예정

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const app = express()
-const port = process.env.PORT || 5500
+// const express = require("express")
+// const cors = require("cors")
+// const bodyParser = require("body-parser")
+// const app = express()
+// const port = process.env.PORT || 5500
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false}))
+// app.use(bodyParser.json())
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: false}))
 
-const Users = require('./routes/Users')
+// const Users = require('./routes/Users')
 
-app.use('/users',Users)
+// app.use('/users',Users)
 
-app.listen(port, ()=> {
-    console.log("Server is running on port: " + port)
-})
+// app.listen(port, ()=> {
+//     console.log("Server is running on port: " + port)
+// })
 
 
 
-/*
+
 const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
@@ -61,4 +61,3 @@ app.post('/api/users', (req,res)=> {
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-*/
