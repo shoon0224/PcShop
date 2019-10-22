426c02b471a316cf7987ed95f1352d8b7426d7dc
shoon0224 <dltkdgns204@naver.com>
2019-10-22 17:26:52 +0900

    디비 프로젝트 참고할겸 와서 라인 정리하고감

--- reactshop/src/components/layout/LayoutHeader.js
+++ reactshop/src/components/layout/LayoutHeader.js
@@ -6,16 +6,16 @@ import PC from '../../Image/PC.jpg'
 const LayoutHeader = () => {
     return (
         <div>
-        <div className="layout">
+            <div className="layout">
 
-            <Link to="/Login" className="item">로그인</Link>
-            <Link to="/Signup" className="item">회원가입</Link>
-            <Link to="/Mypage" className="item">마이페이지</Link>
-            <Link to="/Basket" className="item">장바구니</Link>
-        </div>
-        <div className="pc">
-            <Link  to="/Contents" className="pcitem"><img style={{width:'100px', height:'100px',padding:'50px'}}src={PC} alt="로고"/></Link>
-        </div>
+                <Link to="/Login" className="item">로그인</Link>
+                <Link to="/Signup" className="item">회원가입</Link>
+                <Link to="/Mypage" className="item">마이페이지</Link>
+                <Link to="/Basket" className="item">장바구니</Link>
+            </div>
+            <div className="pc">
+                <Link to="/Contents" className="pcitem"><img style={{ width: '100px', height: '100px', padding: '50px' }} src={PC} alt="로고" /></Link>
+            </div>
         </div>
     )
 }
